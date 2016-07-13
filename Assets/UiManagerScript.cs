f6628cda13d08616c3ee1bcb7a905059502300fa
kloklojul <litwinow@gmx.net>
2016-07-13 23:20:05 +0200

    Alpha Shop 1
    First steps towards a working shop

---
+++ Assets/UiManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum Category {
+    SKIN,POWERUPS,WEAPONS
+}
+public class UiManagerScript : MonoBehaviour {
+    public Category currentCategory;
+    public GameObject[] itemButtons;
+
+	// Use this for initialization
+	void Start () {
+        currentCategory = Category.POWERUPS;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void switchCategory(Category c) {
+        currentCategory = c;
+        foreach (GameObject b in itemButtons) {
+
+        }
+    }
+}
