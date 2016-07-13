ee2cd5f8c414eb5ce6cac64f5dc0fe4fcaebb275
Stardust11 <David.Krah@gmx.de>
2016-07-13 23:17:12 +0200

    Startscreen fixed

--- Assets/TextChangerScript.cs
+++ Assets/TextChangerScript.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
+public enum Texttype {
+    HIGHSCORE, COINS
+}
 public class TextChangerScript : MonoBehaviour {
     private int value;
     private DataManagerScript dm;
+    public Texttype texttype;
 
 	// Use this for initialization
 	void Start () {
         dm = GameObject.FindGameObjectWithTag("DataManager").GetComponent<DataManagerScript>();
-        value = dm.getEndlessHighscore();
-        GetComponent<Text>().text = "Highscore : " + value;
+        switch (texttype) {
+            case Texttype.COINS:
+            value = dm.loadPlayerMoney();
+            GetComponent<Text>().text = "Coins: " + value;
+            break;
+            case Texttype.HIGHSCORE:
+                value = dm.getEndlessHighscore();
+                GetComponent<Text>().text = "Highscore: " + value;
+            break;
+            default:
+            Debug.Log("Texttype not yet implemented");
+            break;
+        }
 	}
 	
 }
